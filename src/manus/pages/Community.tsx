dc2c7697c1f1f2dea0fdaf8f753a5fb81714c748
Alchemyacademy-design <contact@casaalchemystudio.com>
2026-06-26 23:56:20 -0300

    feat(community): activate premium community surface

--- src/manus/pages/Community.tsx
+++ src/manus/pages/Community.tsx
@@ -1,12 +1,12 @@
 import { useSearchParams } from "react-router-dom";
 import MemberLayout from "@/manus/components/MemberLayout";
-import CommunityCenter from "@/manus/components/community/CommunityCenter";
+import CommunityPremium from "@/manus/components/community/CommunityPremium";
 
 export default function Community() {
   const [params] = useSearchParams();
   return (
     <MemberLayout>
-      <CommunityCenter
+      <CommunityPremium
         initialSpaceSlug={params.get("space") ?? undefined}
         initialChannelSlug={params.get("channel") ?? undefined}
         initialDraftTitle={params.get("title") ?? undefined}
